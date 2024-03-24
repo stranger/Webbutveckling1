5c8429872eb8aaaf10cca0c2a3f1869921efede3
stranger <beeyourself1738@gmail.com>
2024-03-24 03:41:30 +0100

    cleaned up nav, intro section test

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -1,9 +1,23 @@
 /* importing normal svg doesn't work doing this for now */
 import * as React from "react";
+
 const Burger = (props) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={18} height={20} {...props}>
     <path stroke="#000" d="M0 2h20M0 10h20M0 18h20" />
   </svg>
 );
 
-export { Burger };
+const Lens = ({ ...props }) => (
+  <div
+    style={{
+      opacity: "0.9",
+      backgroundImage:
+        "radial-gradient(circle at 75% 46%, #FFFFFF 0%, #D5FBFF 50%)",
+      boxShadow: "0 2px 30px -20px rgba(0,0,0,0.50)",
+      borderRadius: "50%",
+    }}
+    {...props}
+  ></div>
+);
+
+export { Burger, Lens };
--- src/pages/landing/landing.jsx
+++ src/pages/landing/landing.jsx
@@ -1,5 +1,18 @@
 import "./landing.css";
 
+import { Lens } from "../../assets/icons";
+
 export default function Landing() {
-  return <section className="landing"></section>;
+  return (
+    <div className="landing">
+      <section className="intro">
+        <Lens className="intro-lens"></Lens>
+        <h1 className="intro-title-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur illum
+          numquam hic sunt aliquam molestiae culpa repudiandae illo quae facere?
+          Tenetur delectus quo culpa? Ipsam quae facere minima soluta obcaecati.
+        </h1>
+      </section>
+    </div>
+  );
 }
