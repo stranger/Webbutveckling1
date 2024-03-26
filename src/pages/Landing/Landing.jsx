aebd2446eaf84f9132e8c3069028000e2372fe1c
stranger <beeyourself1738@gmail.com>
2024-03-26 05:09:35 +0100

    fixed casing

---
+++ src/pages/Landing/Landing.jsx
@@ -0,0 +1,40 @@
+import "./Landing.css";
+
+import { IntroLogo } from "../../assets/icons";
+
+import BlogPost from "./../../components/BlogPost/BlogPost";
+
+import blogData from "./../../assets/blogData.json";
+
+export default function Landing() {
+  return (
+    <div className="landing">
+      <section className="intro-section" id="intro">
+        <div className="intro-text-container">
+          <h1 className="intro-title">WEBBUTVECKLING 1</h1>
+          <h2 className="intro-subtitle">
+            Kursen var en bra crash course för att komma igång med programmering
+            igen efter en lång paus. Jag lärde mig viktiga grundläggande
+            principer.
+          </h2>
+        </div>
+        <IntroLogo className="webb-logo" />
+      </section>
+      <hr className="landing-divider" />
+
+      <section className="blog-section" id="blog">
+        <h3 className="blog-title">BLOGGEN</h3>
+        {/* Itererar över posts arrayen. Data är objekten i arrayen, key är index som react behöver för att hålla koll på vilket element är vad så att den kan re-rendera bara den komponenten istället för alla blog posts */}
+        {blogData.posts.map((data, key) => {
+          return (
+            <BlogPost
+              key={key}
+              blogData={data}
+              className="blog-post"
+            ></BlogPost>
+          );
+        })}
+      </section>
+    </div>
+  );
+}
